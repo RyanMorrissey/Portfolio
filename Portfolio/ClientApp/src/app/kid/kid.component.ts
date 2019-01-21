c866684813884bf7d684181fcda89bc820f5f674
Ryan Morrissey <ryanmorrissey0@gmail.com>
2019-01-21 15:00:15 -0500

    Removed the translation stuff since I am no longer playing around with it

--- Portfolio/ClientApp/src/app/kid/kid.component.ts
+++ Portfolio/ClientApp/src/app/kid/kid.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-kid',
@@ -8,17 +7,10 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class KidComponent implements OnInit {
 
-  constructor(private translate: TranslateService) {
-    translate.setDefaultLang('en');
-    translate.use('en');
-    console.log("test");
+  constructor() {
   }
 
   ngOnInit() {
-  }
-
-  useLanguage(language: string) {
-    this.translate.use(language);
   }
 
 }
